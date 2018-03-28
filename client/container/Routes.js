6522b537edec0b36ad48cc8d8aabad27317d2eb4
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2018-03-28 03:52:56 +0100

    Implement route for Add business

--- client/container/Routes.js
+++ client/container/Routes.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import React from 'react';
 import LandingPage from '../components/LandingPage';
 import BusinessList from '../components/BusinessList';
-// import Login from '../components/Login';
+import AddBusiness from '../components/AddBusiness';
 
 const Routes = () => (
   <BrowserRouter>
     <div>
       <Route exact path="/" component={LandingPage} />
       <Route exact path="/businesses" component={BusinessList} />
-      {/* <Route exact path="/businesses" component={BusinessList} /> */}
+      <Route exact path="/add-business" component={AddBusiness} />
     </div>
   </BrowserRouter>
 );
